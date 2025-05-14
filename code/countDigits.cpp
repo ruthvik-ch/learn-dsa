d77e81cea8ce435f4fbc9996c10ad146e7dfa7b2
ruthvik-ch <chennapragadaruthvik@gmail.com>
2025-05-14 17:16:28 +0530

    Setup and Folder Struc update

--- code/countDigits.cpp
+++ code/countDigits.cpp
@@ -1,15 +1,31 @@
-class Solution {
-public:
-    int countDigit(int n) {
-        int count = 0;
-        if(n==0){
-            return 1;
-        }else{
-            while(n>0){
-                n=n/10;
-                count++;
-            }
-            return count;
-        }
+#include <bits/stdc++.h>
+using namespace std;
+
+int countDigit(int n)
+{
+    int count = 0;
+    if (n == 0)
+    {
+        return 1;
     }
-};
+    else
+    {
+        while (n > 0)
+        {
+
+            n = n / 10;
+            count++;
+        }
+        return count;
+    }
+}
+
+// Default main function
+int main()
+{
+    int n;
+    cin >> n;
+    cout << "No of digits: " << countDigit(n) << endl;
+
+    return 0;
+}
--- code/reversenumber.cpp
+++ code/reversenumber.cpp
@@ -1,11 +1,25 @@
-class Solution {
-public:
-    int reverseNumber(int n) {
-        int rev = 0;
-        while(n>0){
-            rev = rev*10 + n%10;
-            n = n/10;
-        }
-        return rev;
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int reverseNumber(int n)
+{
+    int rev = 0;
+    while (n > 0)
+    {
+        rev = rev * 10 + n % 10;
+        n = n / 10;
     }
-};
+    return rev;
+}
+
+// Default main function
+int main()
+{
+    int n;
+    cin >> n;
+    cout << "Reversed number: " << reverseNumber(n) << endl;
+
+    return 0;
+}
